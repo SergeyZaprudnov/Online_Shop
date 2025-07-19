94ba559881df75daedaab5a07384d754b21da832
sergey <szaprudnov@hotmail.com>
2025-07-19 19:36:06 +0300

    representations registered

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from products.models import Category, Product
+
+
+@admin.register(Category)
+class CategoryModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+
+@admin.register(Product)
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', 'description', 'price', 'image',)
